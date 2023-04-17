af5e4f2c064298c15ce56f394efa4f295147e342
SREERAJ R <84067685+sreeraj6@users.noreply.github.com>
2023-04-17 18:35:06 +0530

    Docker created for node

--- routes/admin.js
+++ routes/admin.js
@@ -9,29 +9,43 @@ var fs = require("fs");
 const adminAuth = require('../Helpers/adminHelper/adminAuth');
 var router = express.Router();
 
+const verifyLogin = (req, res, next) => {
+    req.session.loginError = false
+    if (req.session.loggedIn) {
+      username = req.session.user.Name,
+      next()
+    } else {
+      res.redirect('/admin/login')
+    }
+  }
+
 //GET admin login form
 //route admin/login
-router.get('/', (req, res) => {
+router.get('/',verifyLogin, (req, res) => {
     res.render('admin/home.hbs',{admin:true})
 })
 
 //GET admin login form
 //route admin/login
 router.get('/login', (req, res) => {
-    res.render('admin/login.hbs')
+    res.render('admin/login')
 });
 
 //POST admin login submint
 //route admin/login
 router.post('/login', (req, res) => {
-    req.body.type = 2;
+    req.body.type = 1;
     adminAuth.doLogin(req.body).then((response)=>{
-        if(response.status){
+        if (response.status) {
+            req.session.loginError = false
+            req.session.loggedIn = true
+            req.session.user = response.user
             res.redirect('/admin')
-        }
-        else{
-            res.redirect('/login')
-        }
+          }
+          else {
+            req.session.loginError = true
+            res.redirect('/admin/login')
+          }
     })
    
 })
@@ -45,7 +59,7 @@ router.get('/add-staff',async (req, res) => {
 
 //POST admin add staff form
 //@DESC store staff data
-router.post('/add-staff', (req, res) => {
+router.post('/add-staff',verifyLogin, (req, res) => {
     var dept = req.body.sfDept.split(",");
     req.body.deptId = dept[0];
     req.body.deptName = dept[1];
@@ -63,7 +77,7 @@ router.get('/check',(req,res)=>{
     // console.log(Date.UTC());
     res.send(new Date)
 })
-router.get('/staff-data',(req,res)=>{
+router.get('/staff-data',verifyLogin, (req,res)=>{
     
     staffController.getstaffData().then((staffData)=>{
         res.render('admin/staff-data',{staffdata:staffData,admin:true})
@@ -72,7 +86,7 @@ router.get('/staff-data',(req,res)=>{
     
 //GET admin add student form
 //route admin/add-student
-router.get('/add-student',async (req, res) => {
+router.get('/add-student',verifyLogin, async (req, res) => {
     var dept = await departmentController.getDepartment();
     console.log(dept);
     res.render('admin/add-student.hbs',{admin:true,dept})
@@ -81,38 +95,38 @@ router.get('/add-student',async (req, res) => {
 
 //POST store student details
 //@route admin/add-student
-router.post('/add-student', (req, res) => {
+router.post('/add-student',verifyLogin, (req, res) => {
     studentController.addStudent(req.body).then((response) => {
         if (response.user) {
             req.session.student = true;
-            res.redirect('/admin/add-student')
+            res.redirect('/admin/add-student', {admin:true})
         }
         else{
-            res.redirect('/admin/add-student');
+            res.redirect('/admin/add-student', {admin:true});
         }
     })
      
 })
 
 //get student
-router.get('/get-student', (req, res) => {
+router.get('/get-student',verifyLogin, (req, res) => {
     studentController.getStudent().then((students)=>{
-        res.render('admin/student-data',{students});
+        res.render('admin/student-data',{admin:true, students});
     })
 })
 //dept -> add-dept, add-syllab,
 //GET admin add dept form and assign hod
 //route admin/add-dept
-router.get('/add-dept', (req,res) => { //required : name, capacity, semester, 
+router.get('/add-dept',verifyLogin, (req,res) => { //required : name, capacity, semester, 
 //for assigning hod take all the staff of the current dept and display in dropdown
 res.render('admin/add-dept',{admin:true})
 })
 
-router.post('/add-dept', (req,res) => { 
+router.post('/add-dept',verifyLogin, (req,res) => { 
  departmentController.addDept(req.body).then((response)=>{
     if (response.user) {
         
-        res.redirect('/admin/add-dept')
+        res.redirect('/admin/add-dept',{admin:true,})
     }
     else{
         res.redirect('/admin');
@@ -121,24 +135,24 @@ router.post('/add-dept', (req,res) => {
 })
 
 //show department and current hods
-router.get('/get-dept', (req, res) => {
+router.get('/get-dept',verifyLogin, (req, res) => {
     departmentController.getDepartment().then((deptdata) => {
-        res.render('admin/department-data',{deptdata});
+        res.render('admin/department-data',{admin:true, deptdata});
     }) 
 })
 
 
 //update department
-router.get('/update-dept/:id',async (req, res) => {
+router.get('/update-dept/:id',verifyLogin, async (req, res) => {
     var staff = await staffController.getDeptStaff(req.params.id);
     departmentController.getDepartmentId(req.params.id).then((deptInfo)=>{
-        res.render('admin/edit-dept',{deptInfo,staff})
+        res.render('admin/edit-dept',{admin:true, deptInfo,staff})
     })
 })
 
 
 //update department
-router.post('/update-dept/:id',async (req, res) => {
+router.post('/update-dept/:id',verifyLogin, async (req, res) => {
     var staff = req.body.hod.split(",");
     req.body.hodId = staff[0];
     req.body.hodName = staff[1];
@@ -161,23 +175,23 @@ router.post('/update-dept/:id',async (req, res) => {
 
 //@DESC get add subj form
 //@GET add-subject
-router.get('/add-sub', (req, res) => {
+router.get('/add-sub',verifyLogin, (req, res) => {
     departmentController.getDepartment().then((dept) =>{
-        res.render('admin/add-sub',{dept});
+        res.render('admin/add-sub',{admin:true, dept});
     })
    
 })
 
 
 //@POST /admin/add-sub
-router.post('/add-sub', (req, res) => {
+router.post('/add-sub',verifyLogin, (req, res) => {
     subjectController.addSubject(req.body).then((response) => {
         if(response.status){
             res.redirect('/admin/add-sub');
         }
         else{
             console.log(response);
-            res.render('error',{message:response.code});
+            res.render('error',{admin:true, message:response.code});
         }
     })
 })
@@ -185,19 +199,19 @@ router.post('/add-sub', (req, res) => {
 
 //addsyllabus
 
-router.get('/add-syllab',async(req, res) => {
+router.get('/add-syllab',verifyLogin, async(req, res) => {
     var dept = await departmentController.getDepartment();
-    res.render('admin/add-syllab',{dept})
+    res.render('admin/add-syllab',{admin:true, dept})
 })
 
 
-router.post('/add-syllab', (req, res) => {
+router.post('/add-syllab',verifyLogin, (req, res) => {
     departmentController.addSyllabus(req.body).then((response)=>{
         res.redirect('/admin/add-syllabus')
     })
 })
 
-router.get('/get-sub/:id', async (req,res) => {
+router.get('/get-sub/:id',verifyLogin, async (req,res) => {
     var subect = await subjectController.getSubject(req.params.id);
 
     res.json(subect);
@@ -208,16 +222,16 @@ router.get('/get-sub/:id', async (req,res) => {
 
 //DESC add hall form
 //GET /admin/add-hall
-router.get('/add-hall',(req, res) => {
-    res.render('admin/add-hall');
+router.get('/add-hall',verifyLogin, (req, res) => {
+    res.render('admin/add-hall', {admin:true,});
 })
 
 
 //DESC add hall form
 //POST /admin/add-hall
-router.post('/add-hall',(req, res) => {
+router.post('/add-hall',verifyLogin, (req, res) => {
     examHallController.addHall(req.body).then((response)=>{
-        res.redirect('/admin/add-hall');
+        res.redirect('/admin/add-hall',{admin:true,});
     })
 })
 
@@ -225,12 +239,12 @@ router.post('/add-hall',(req, res) => {
 //2.select dept,hall,mixwithK(no:of students in a bench) and allocate exam hall  
 //DESC exam hall allocate form
 //GET /admin/generate-allocation
-router.get('/generate-allocation',(req, res) => {
-    res.render('admin/examHall');
+router.get('/generate-allocation',verifyLogin, (req, res) => {
+    res.render('admin/examHall',{admin:true,});
 })
 
 
-router.post('/generate-allocation', (req,res) => {
+router.post('/generate-allocation',verifyLogin, (req,res) => {
     var arr = req.body.halls.split(",");
     examHallController.generateAllotment(req.body,arr).then((response)=>{
         console.log(res);
